d245403d6aae3f7585f6c74d16c7fed65e76f739
tdharris <tylerdavidharris@gmail.com>
2014-07-26 09:25:01 -0600

    better app logging for crashes/restarts

--- startup.js
+++ startup.js
@@ -1,16 +1,34 @@
 var logRotate = require('./js/logRotate'),
     config = require('./config.json'),
     startStopDaemon = require('start-stop-daemon'),
-    startApp = require('./app');
+    startApp = require('./app'),
+    logme = require('logme');
 
 logRotate();
 
 startStopDaemon({
+	'killTree': true,
     'max': 3,
     'append': true,
     'logFile': config.logDirectory + '/app.log'
     // 'outFile': config.logDirectory + '/app.log',
     // 'errFile': config.logDirectory + '/app.log'
 }, function() {
-    startApp();
-});
+	startApp();
+})
+
+	.on('restart', function() {
+		logme.info('Restarting at ' + new Date()); 
+	})
+
+	.on('start', function() {
+		logme.info('Starting at ' + new Date()); 
+	})
+
+	.on('stop', function() {
+		logme.info('Stopping at ' + new Date()); 
+	})
+
+	.on('exit', function() {
+		logme.info('Exiting at ' + new Date()); 
+	});
